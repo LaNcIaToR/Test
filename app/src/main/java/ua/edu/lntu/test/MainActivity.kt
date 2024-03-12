87f91726d59e68c2d2fc3cf24f5185543765b1f8
LaNcIaToR <jonas7238@icloud.com>
2024-03-12 11:02:29 +0200

    Maked function "SliderPage" for display images

--- app/src/main/java/ua/edu/lntu/test/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/test/MainActivity.kt
@@ -3,25 +3,33 @@ package ua.edu.lntu.test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentComposer
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ua.edu.lntu.test.ui.theme.TestTheme
 
@@ -45,7 +53,47 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp()
 {
-    CounterPage()
+    //CounterPage()
+    SliderPage()
+}
+
+@Composable
+fun SliderPage()
+{
+    var imageList = listOf(R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground)
+    var currentIndex by remember { mutableStateOf(0) }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Image(
+            painter = painterResource(imageList[currentIndex]),
+            contentDescription = null,
+            modifier = Modifier
+                .size(200.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ){
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { if(currentIndex > 0) currentIndex--}) {
+                Text("Previous")
+            }
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = { if(currentIndex < imageList.size - 1) currentIndex++}) {
+                Text("Next")
+            }
+        }
+    }
 }
 
 @Composable
